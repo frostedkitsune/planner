22882e2fd8f462aa1ca1a8fc7f5df258b78b3037
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2024-02-03 02:23:33 +0530

    adding accounts route

---
+++ routes/accounts.tsx
@@ -0,0 +1,12 @@
+import { Partial } from "$fresh/runtime.ts";
+
+export default function Accounts() {
+    return (
+        <div className="w-screen h-screen grid place-items-center bg-green-500" f-client-nav>
+            hh
+            <Partial name="sign">
+
+            </Partial>
+        </div>
+    )
+}

3eeb48b3dc70eba8b87c2478e08f2dc130340ef0
Ss3347C <134444582+Ss3347C@users.noreply.github.com>
2024-02-03 03:49:26 +0530

    input comp

--- routes/createmap.tsx
+++ routes/createmap.tsx
@@ -1,3 +1,5 @@
+import TextInput from "../components/TextInput.tsx";
+
 export default function createmap() {
   return (
     <>
@@ -5,6 +7,7 @@ export default function createmap() {
         <div className="w-6/12 h-auto bg-[--pri] flex justify-center p-[10px]">
           <div className="name flex w-52 h-28 bg-[--sec] flex-col p-[10px] rounded-[--brds]">
             <p class="mb-2.5">Hi! What is your Name?</p>
+            <TextInput name="mk" label="Enter a Course Name "/>
             <input type="text" placeholder="Enter name here" className="bg-[--sec] no-underline outline-0 border-none"/>
             <hr className="text-[--pri]"/>
           </div>

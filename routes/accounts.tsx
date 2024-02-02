748e30d585bb8d97ae546d7624b28471c1b22209
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2024-02-03 02:35:44 +0530

    fix acc

--- routes/accounts.tsx
+++ routes/accounts.tsx
@@ -1,12 +1,12 @@
 import { Partial } from "$fresh/runtime.ts";
 
 export default function Accounts() {
-    return (
-        <div className="w-screen h-screen grid place-items-center bg-green-500" f-client-nav>
-            hh
-            <Partial name="sign">
-
-            </Partial>
-        </div>
-    )
+  return (
+    <div className="w-screen h-screen grid place-items-center bg-[--pri]">
+      <div className="w-2/5 h-2/4 bg-[--sec] plc-border" f-client-nav>
+        <Partial name="sign">
+        </Partial>
+      </div>
+    </div>
+  );
 }

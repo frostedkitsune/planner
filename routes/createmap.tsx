7e3931812a1f7a61c2de0b989bbef6db990eb9b4
Ss3347C <134444582+Ss3347C@users.noreply.github.com>
2024-02-03 03:15:04 +0530

    updated

--- routes/createmap.tsx
+++ routes/createmap.tsx
@@ -1,9 +1,15 @@
 export default function createmap() {
   return (
     <>
-        <div className="name w-52 h-40 border">
-
+      <div className="w-screen h-screen flex justify-center p-[40px]">
+        <div className="w-6/12 h-auto bg-[--pri] flex justify-center p-[10px]">
+          <div className="name flex w-52 h-28 bg-[--sec] flex-col p-[10px] rounded-[--brds]">
+            <p class="mb-2.5">Hi! What is your Name?</p>
+            <input type="text" placeholder="Enter name here" className="bg-[--sec] no-underline outline-0 border-none"/>
+            <hr className="text-[--pri]"/>
+          </div>
         </div>
+      </div>
     </>
   );
 }

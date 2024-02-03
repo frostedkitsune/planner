4ca7448ffc68fa637f594556656fcac7e56fd2cc
ss3347C <134444582+Ss3347C@users.noreply.github.com>
2024-02-04 03:28:22 +0530

    fix design

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -9,8 +9,8 @@ import * as $api_joke from "./routes/api/joke.ts";
 import * as $createmap from "./routes/createmap.tsx";
 import * as $dashboard from "./routes/dashboard.tsx";
 import * as $discover from "./routes/discover.tsx";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
+import * as $map_id_ from "./routes/map/[id].tsx";
 import * as $progress from "./routes/progress.tsx";
 import * as $Counter from "./islands/Counter.tsx";
 import * as $Resource from "./islands/Resource.tsx";
@@ -26,8 +26,8 @@ const manifest = {
     "./routes/createmap.tsx": $createmap,
     "./routes/dashboard.tsx": $dashboard,
     "./routes/discover.tsx": $discover,
-    "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
+    "./routes/map/[id].tsx": $map_id_,
     "./routes/progress.tsx": $progress,
   },
   islands: {

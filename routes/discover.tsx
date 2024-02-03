928493f4d00eb3b1785e6b744619e42a0d1b1c60
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2024-02-03 09:14:25 +0530

    added discover route

---
+++ routes/discover.tsx
@@ -0,0 +1,9 @@
+export default function Discover() {
+    return (
+        <div id="discover-wrap" className="grid">
+            <div id="top-nav"></div>
+            <div id="filter"></div>
+            <div id="dashboard"></div>
+        </div>
+    );
+}

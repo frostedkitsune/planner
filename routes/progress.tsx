80caad23fd0f3f4842a54864e152ab102f98f6f3
Eternal Green <eternal.green@outlook.com>
2024-02-04 02:57:28 +0530

    Added Magic Hover

--- routes/progress.tsx
+++ routes/progress.tsx
@@ -1,8 +1,5 @@
 const progress = () => {
   return (
-    // <section
-    //   className={"w-screen min-h-screen grid place-items-center bg-[--pri] py-20 overflow-x-hidden"}
-    // >
     <div className="w-screen min-h-screen py-12 bg-[--pri] relative">
       <h1 className={"text-center text-3xl font-bold mb-10"}>
         React Roadmap
@@ -21,42 +18,68 @@ const progress = () => {
         </div>
 
         <div className="grid grid-cols-5 gap-y-12 w-full mt-24 px-44 text-xl">
-          <div class="bg-[--sec] col-start-1 col-span-2 h-56 rounded-[--brds] w-full plc-border">
-            <div
-              className={"p-2 rounded-full bg-[--ace] text-white font-bold h-16 w-16 grid place-content-center -mt-6 -ml-6"}
-            >
-              01
+          <div class="bg-[--sec] col-start-1 col-span-2 h-56 rounded-[--brds] w-full plc-border relative group">
+            <div class="p-2 rounded-full bg-[--ace] group-hover:bg-green-500 text-white font-bold h-16 w-16 grid place-content-center -mt-6 -ml-6">
+              <span
+                class={"group-hover:hidden transition-opacity duration-300"}
+              >
+                01
+              </span>
+              <span class="hidden opacity-0 group-hover:opacity-100 group-hover:block transition-opacity duration-300">
+                ✔
+              </span>
             </div>
-            <div className="grid place-content-center w-full mt-12">
+            <div class="grid place-content-center w-full mt-12">
               useState
             </div>
           </div>
 
-          <div class="bg-[--sec] col-start-4 col-span-2 h-56 rounded-[--brds] w-full plc-border">
+          <div class="bg-[--sec] col-start-4 col-span-2 h-56 rounded-[--brds] w-full plc-border group">
             <div
-              className={"p-2 rounded-full bg-[--ace] text-white font-bold h-16 w-16 grid place-content-center -mt-6 -ml-6"}
+              className={"p-2 rounded-full bg-[--ace] group-hover:bg-green-500 text-white font-bold h-16 w-16 grid place-content-center -mt-6 -ml-6"}
             >
-              02
+              <span
+                class={"group-hover:hidden transition-opacity duration-300"}
+              >
+                02
+              </span>
+              <span class="hidden opacity-0 group-hover:opacity-100 group-hover:block transition-opacity duration-300">
+                ✔
+              </span>
             </div>
             <div className="grid place-content-center w-full mt-12">
               Custom Hooks
             </div>
           </div>
-          <div class="bg-[--sec] col-start-2 col-span-2 h-56 w-full rounded-[--brds] plc-border">
+          <div class="bg-[--sec] col-start-2 col-span-2 h-56 w-full rounded-[--brds] plc-border group">
             <div
-              className={"p-2 rounded-full bg-[--ace] text-white font-bold h-16 w-16 grid place-content-center -mt-6 -ml-6"}
+              className={"p-2 rounded-full bg-[--ace] group-hover:bg-red-600 text-white font-bold h-16 w-16 grid place-content-center -mt-6 -ml-6"}
             >
-              03
+              <span
+                class={"group-hover:hidden transition-opacity duration-300"}
+              >
+                03
+              </span>
+              <span class="hidden opacity-0 group-hover:opacity-100 group-hover:block transition-opacity duration-300">
+              ✗
+              </span>
             </div>
             <div className="grid place-content-center w-full mt-12">
               State Management
             </div>
           </div>
-          <div class="bg-[--sec] col-start-3 col-span-2 h-56 w-full rounded-[--brds] plc-border">
+          <div class="bg-[--sec] col-start-3 col-span-2 h-56 w-full rounded-[--brds] plc-border group">
             <div
-              className={"p-2 rounded-full bg-[--ace] text-white font-bold h-16 w-16 grid place-content-center -mt-6 -ml-6"}
+              className={"p-2 rounded-full bg-[--ace] group-hover:bg-red-600 text-white font-bold h-16 w-16 grid place-content-center -mt-6 -ml-6"}
             >
-              04
+              <span
+                class={"group-hover:hidden transition-opacity duration-300"}
+              >
+                04
+              </span>
+              <span class="hidden opacity-0 group-hover:opacity-100 group-hover:block transition-opacity duration-300">
+              ✗
+              </span>
             </div>
             <div className="grid place-content-center w-full mt-12">
               Api Calls

2273ea7552d222a2059b49bc202ff2e5d48c7648
Ss3347C <134444582+Ss3347C@users.noreply.github.com>
2024-02-04 01:15:56 +0530

    test

--- islands/Resource.tsx
+++ islands/Resource.tsx
@@ -3,24 +3,31 @@ export default function Resource() {
   const add = () => {
     const input: any = document.getElementById("input");
     const ul: any = document.getElementById("ul");
-    const ele = `<li><a href="${input.value}">${input.value}</a></li>`;
+    console.log(ul);
+    const ele: any = (
+      <li className="plc-li">
+        <a href={input.value}>{input.value}</a>
+      </li>
+    );
+    
     if (input.value != "") {
-      ul.innerHTML += ele;
       console.log(ele);
-      arr.push(ele)
+      // ul.append(ele);
+      arr.push(ele);
     }
   };
 
   return (
     <div className="bg-[--pri] w-[300px] rounded-[--brds]  mt-2 p-2">
       <label>Add Resources</label>
-      <div className="bg-white items p-2 mt-2 rounded-[--brds] overflow-scroll max-h-24 h-24  hover:outline outline-2 outline-[--txt]">
+      <div className="items p-2 mt-2 rounded-[--brds] overflow-scroll max-h-24 h-24">
         <ul id="ul">
+          {arr}
         </ul>
       </div>
       <div className="mt-2 flex">
         <input
-        placeholder="Enter here"
+          placeholder="Enter here"
           type="text"
           id="input"
           className="w-9/12 p-1 pl-2 focus:outline active:outline hover:outline outline-2 outline-[--txt] rounded-[--brds] mr-2"
@@ -32,9 +39,11 @@ export default function Resource() {
           >
             +
           </button>
-          {/* <button className=" rounded-[--brds] w-[25px] h-[25px] focus:outline active:outline hover:outline outline-2 outline-[--txt]   ">
+          {
+            /* <button className=" rounded-[--brds] w-[25px] h-[25px] focus:outline active:outline hover:outline outline-2 outline-[--txt]   ">
             -
-          </button> */}
+          </button> */
+          }
         </div>
       </div>
     </div>

60d9055f1732e0ffb7a94b7d744aecc49009c125
Eternal Green <eternal.green@outlook.com>
2024-02-04 11:42:58 +0530

    index page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,30 @@
-export default function index()
-{
-  return(
-    <div id="palette" class="border rounded-[--brds]">
+export default function index() {
+  return (
+    <div
+      id="palette"
+      class="py-44 px-24 flex flex-row justify-center items-center"
+    >
+      <div className={"w-[50%]"}>
+        <h1 class="text-[3.50rem] font-bold text-gray-900">
+          Welcome to{" "}
+          <span className={"bg-[--sec] p-2 text-[--txt] rounded-[--brds]"}>
+            Planner !
+          </span>
+        </h1>
+        <p class={"mt-8 text-xl mr-36"}>
+          Create learning paths, track your progress and collaborte with others
+          to create a better learing environment
+        </p>
+      </div>
+      <div className={"flex flex-col ml-20"}>
+        <h2 class={"text-3xl font-bold my-6"}>Join Today !</h2>
+        <a
+          href="/accounts"
+          class={"bg-[--ace] p-3 text-xl w-fit rounded-[--brds] text-[--pri] mx-auto"}
+        >
+          Sign up
+        </a>
+      </div>
     </div>
-  )
+  );
 }
